ea69dfce771ac808de56a0c705e2021cdbabb2a7
Gerardo Sullivan <g_j_sullivan@hotmail.com>
2019-09-28 22:23:56 +1200

    Add api solution

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+// import * as NaturalLanguageUnderstandingV1 from 'ibm-watson/natural-language-understanding/v1';
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
@@ -9,28 +10,47 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+// const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+//     version: '2019-07-12',
+//     iam_apikey: 'DQu1eofIByqgFceEocGYtM6STy4TrjZ07RgtBWXehzNP',
+//     url: 'https://gateway-syd.watsonplatform.net/natural-language-understanding/api'
+// });
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const addUrl = functions.https.onRequest((request, response) => {
-
-    // TODO: Http call to IBM Watson followed by saving returned data to firestore
-    response.send("Hello World");
-});
-
 // export const addUrl = functions.https.onRequest((request, response) => {
-//     const url = request.body.url;
-//     console.log("url:" + url);
-
-//     db.collection
 
 //     // TODO: Http call to IBM Watson followed by saving returned data to firestore
-//     response.send("You sent " + url);
+//     response.send("Hello World");
+// });
+
+// export const addUrl = functions.https.onRequest((request, response) => {
+//     const analyzeParams = {
+//         'url': request.body['url'],
+//         'features': {
+//             'categories': {},
+//             'concepts': {},
+//             'keywords': {},
+//             'metadata': {}
+//         }
+//     };
+
+//     naturalLanguageUnderstanding.analyze(analyzeParams)
+//         .then(analyzeResult => {
+//             console.log(analyzeResult);
+//             response.send(analyzeResult);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             response.status(500).send(error);
+//         });
+
 // });
 
 export const getUser = functions.https.onRequest((request, response) => {
     db.collection("users").get()
-        .then(snapshot => response.send(snapshot.docs.forEach(document => document.data)))
+        .then(snapshot => { response.send(snapshot.docs.forEach(document => document.data)); })
         .catch(error => {
             console.log(error);
             response.status(500).send(error);
