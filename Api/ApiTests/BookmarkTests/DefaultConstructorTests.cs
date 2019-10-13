e7c98ad507c55b1014542ea0b51216d88bf475e2
Gerardo Sullivan <g_j_sullivan@hotmail.com>
2019-10-13 18:48:06 +1300

    Add tests

---
+++ Api/ApiTests/BookmarkTests/DefaultConstructorTests.cs
@@ -0,0 +1,52 @@
+﻿using Api.Models.Firestore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTests.BookmarkTests
+{
+    [TestClass]
+    public class DefaultConstructorTests
+    {
+        [TestMethod]
+        public void CategoriesTest()
+        {
+            var bookmark = new Bookmark();
+
+            Assert.IsNotNull(bookmark.Categories);
+        }
+
+        [TestMethod]
+        public void ConceptsTest()
+        {
+            var bookmark = new Bookmark();
+
+            Assert.IsNotNull(bookmark.Concepts);
+        }
+
+        [TestMethod]
+        public void KeywordsTest()
+        {
+            var bookmark = new Bookmark();
+
+            Assert.IsNotNull(bookmark.Keywords);
+        }
+
+        [TestMethod]
+        public void TagsTest()
+        {
+            var bookmark = new Bookmark();
+
+            Assert.IsNotNull(bookmark.Tags);
+        }
+
+        [TestMethod]
+        public void CreatedTimeTest()
+        {
+            var bookmark = new Bookmark();
+
+            Assert.IsNotNull(bookmark.CreatedTime);
+        }
+    }
+}
